be6c92ecb757f776e9251362fb4d8e2cd1748303
MiniPiku <21parthib2006@gmail.com>
2025-10-20 17:35:31 +0530

    implement

--- activityservice/src/main/java/org/minipiku/activityservice/Config/WebClientConfig.java
+++ activityservice/src/main/java/org/minipiku/activityservice/Config/WebClientConfig.java
@@ -8,18 +8,17 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    //TODO: fix this ("http://localhost:8081")-->("http://USER-SERVICE")
-    /*
     @Bean
     @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
-    */
+
     @Bean
     public WebClient userServiceWebClient(WebClient.Builder builder) {
         return builder
-                .baseUrl("http://localhost:8081")
+                .baseUrl("lb://USER-SERVICE")  // or "http://USER-SERVICE"
                 .build();
     }
 }
+
